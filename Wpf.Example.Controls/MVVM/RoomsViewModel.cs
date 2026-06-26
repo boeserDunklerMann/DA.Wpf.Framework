69713c292fb4e14d10f2ac181158a0a36d5cec6a
André <andre.dunkel@t-online.de>
2026-06-26 15:52:05 +0200

    Rooms added

---
+++ Wpf.Example.Controls/MVVM/RoomsViewModel.cs
@@ -0,0 +1,88 @@
+﻿using DA.SharedDeskPlanner.Model;
+using DA.SharedDeskPlanner.Model.Contracts;
+using DA.Wpf.Framework;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Wpf.Example.Controls.MVVM
+{
+	/// <ChangeLog>
+	/// <Create Datum="26.06.2026" Entwickler="DA" />
+	/// </ChangeLog>
+	internal class RoomsViewModel(ISharedDeskPlannerContext ctx, IDialogService dialog) : BaseViewModel(ctx)
+	{
+		private readonly ObservableCollection<Room> _rooms = [];
+		public ObservableCollection<Room> Rooms => _rooms;
+		private Room? _selectedRoom;
+		public Room? SelectedRoom
+		{
+			get => _selectedRoom;
+			set
+			{
+				_selectedRoom = value;
+				RaisePropChanged(nameof(SelectedRoom));
+			}
+		}
+		private Room _newRoom = BaseModel.Create<Room>();
+		public Room NewRoom => _newRoom;
+
+		#region Overrides from BaseViewModel
+		public override async Task OnInitAsync()
+		{
+			await LoadRoomsAsync();
+		}
+
+		public override async void OnStart()
+		{
+			await Task.CompletedTask;
+		}
+
+		public override async void OnStop()
+		{
+			await Task.CompletedTask;
+		}
+		#endregion
+		#region private methods
+		private async Task LoadRoomsAsync()
+		{
+			if (dbcontext != null)
+			{
+				var rooms = await dbcontext.Rooms
+					.Include(nameof(Room.Desks))
+					.Where(r => !r.Deleted).ToListAsync();
+				_rooms.Clear();
+				rooms.ForEach(_rooms.Add);
+				RaisePropChanged(nameof(Rooms));
+			}
+			else
+				dialog.ShowError("kein DB Context vorhanden");
+		}
+		#endregion
+		#region Commands
+		public DelegateCommand DeleteRoom => new DelegateCommand(CmdDeleteRoom);
+		#endregion
+		#region Actions
+		private async void CmdDeleteRoom()
+		{
+			try
+			{
+				if (dbcontext != null && _selectedRoom != null)
+				{
+					_selectedRoom.Deleted = true;
+					await dbcontext.SaveChangesAsync();
+					await LoadRoomsAsync();
+				}
+				else
+					dialog.ShowError($"DB Context oder {nameof(_selectedRoom)} nicht vorhanden");
+			}
+			catch (Exception ex)
+			{
+				dialog.ShowError(ex.Message);
+			}
+		}
+		#endregion
+	}
+}
---
+++ Wpf.Example.Controls/Rooms.xaml.cs
@@ -0,0 +1,57 @@
+﻿using DA.SharedDeskPlanner.Model.Contracts;
+using DA.Wpf.Framework;
+using DA.Wpf.Framework.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Example.Controls.MVVM;
+
+namespace Wpf.Example.Controls
+{
+	/// <ChangeLog>
+	/// <Create Datum="26.06.2026" Entwickler="DA" />
+	/// </ChangeLog>
+	/// <summary>
+	/// Interaction logic for Rooms.xaml
+	/// </summary>
+	[RibbonTabControl(heading:"Räume", position:3)]
+	public partial class Rooms : UserControl, IRibbonTabControl
+	{
+		private readonly RoomsViewModel vm;
+		public Rooms(ISharedDeskPlannerContext ctx, IDialogService dlgService)
+		{
+			InitializeComponent();
+			vm = new RoomsViewModel(ctx, dlgService);
+			DataContext = vm;
+		}
+
+		public RibbonTabControlAttribute GetAttribute()
+			=> GetType().GetCustomAttribute<RibbonTabControlAttribute>()!;
+
+		public async Task OnInitAsync(IServiceCollection services)
+		{
+			await vm.OnInitAsync();
+		}
+
+		public void OnStart()
+		{
+			vm.OnStart();
+		}
+
+		public void OnStop()
+		{
+			vm.OnStop();
+		}
+	}
+}
