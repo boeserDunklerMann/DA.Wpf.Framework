0199c109df0461a28ba10df61ceb0e0a1aeb7e4a
André <andre.dunkel@t-online.de>
2026-06-28 11:28:33 +0200

    CurrentUserService added

---
+++ DA.Wpf.Framework/Auth/CurrentUserService.cs
@@ -0,0 +1,22 @@
+﻿using DA.SharedDeskPlanner.Model;
+
+namespace DA.Wpf.Framework.Auth
+{
+	/// <ChangeLog>
+	/// <Create Datum="28.06.2026" Entwickler="DA" />
+	/// </ChangeLog>
+	public class CurrentUserService : ICurrentUserService
+	{
+		public User? IsLoggedInUser { get; private set; }
+
+		public void ClearSession()
+		{
+			IsLoggedInUser = null;
+		}
+
+		public void SetCurrentUser(User user)
+		{
+			IsLoggedInUser = user ?? throw new ArgumentNullException(nameof(user));
+		}
+	}
+}
---
+++ DA.Wpf.Framework/Auth/ICurrentUserService.cs
@@ -0,0 +1,15 @@
+﻿using DA.SharedDeskPlanner.Model;
+
+namespace DA.Wpf.Framework.Auth
+{
+	/// <ChangeLog>
+	/// <Create Datum="28.06.2026" Entwickler="DA" />
+	/// </ChangeLog>
+	public interface ICurrentUserService
+	{
+		User? IsLoggedInUser { get; }
+		bool IsAuthenticated => IsLoggedInUser != null;
+		void SetCurrentUser(User user);
+		void ClearSession();
+	}
+}
