50cc86f4d85f8903b9dc565d6f1eadaf48e847d4
André <andre.dunkel@t-online.de>
2026-06-28 07:13:23 +0200

    Dashboard added

---
+++ Wpf.Example.Controls/Dashboard.xaml.cs
@@ -0,0 +1,41 @@
+﻿using DA.SharedDeskPlanner.Model.Contracts;
+using DA.Wpf.Framework;
+using DA.Wpf.Framework.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.Windows.Controls;
+using Wpf.Example.Controls.MVVM;
+
+namespace Wpf.Example.Controls
+{
+	/// <ChangeLog>
+	/// <Create Datum="28.06.2026" Entwickler="DA" />
+	/// </ChangeLog>
+	/// <summary>
+	/// Interaction logic for Dashboard.xaml
+	/// </summary>
+	[RibbonTabControl(heading:"Dashboard", position:0)]
+	public partial class Dashboard : UserControl, IRibbonTabControl
+	{
+		private readonly DashboardViewModel vm;
+		public Dashboard(ISharedDeskPlannerContext ctx, IDialogService dialogService)
+		{
+			InitializeComponent();
+			vm = new(ctx, dialogService);
+			DataContext = vm;
+
+		}
+
+		public RibbonTabControlAttribute GetAttribute()
+			=> GetType().GetCustomAttribute<RibbonTabControlAttribute>()!;
+
+		public async Task OnInitAsync(IServiceCollection services)
+		{
+			await vm!.OnInitAsync();
+		}
+
+		public void OnStart() => vm!.OnStart();
+
+		public void OnStop() => vm!.OnStop();
+	}
+}
---
+++ Wpf.Example.Controls/MVVM/DashboardViewModel.cs
@@ -0,0 +1,27 @@
+﻿using DA.SharedDeskPlanner.Model.Contracts;
+using DA.Wpf.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wpf.Example.Controls.MVVM
+{
+	/// <ChangeLog>
+	/// <Create Datum="28.06.2026" Entwickler="DA" />
+	/// </ChangeLog>
+	internal class DashboardViewModel(ISharedDeskPlannerContext ctx, IDialogService dialogService) : BaseViewModel(ctx)
+	{
+		public override async Task OnInitAsync()
+		{
+			await Task.CompletedTask;
+		}
+
+		public override void OnStart()
+		{
+		}
+
+		public override void OnStop()
+		{
+		}
+	}
+}
